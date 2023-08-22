b5254583511c2fec21d83ba22f35b172105fcd30
Farid-Hamid-4 <faridhamid00@gmail.com>
2023-08-22 18:59:13 -0400

    Finished the skills icons order and animations

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import "bootstrap/dist/css/bootstrap.css";
-import { useState } from "react";
-import { Image } from "react-bootstrap";
 import "devicon/devicon.min.css";
+import { Image } from "react-bootstrap";
+import Icon from "./Icon";
 import me from "./me_cropped.png";
 import "./App.css";
 
 
 export default function App() {
-    const [isHovered, setIsHovered] = useState(false);
-
-    const handleMouseEnter = () => {
-      setIsHovered(true);
-    };
-  
-    const handleMouseLeave = () => {
-      setIsHovered(false);
-    };
+    const iconsList = {
+        "c-plain": "C", 
+        "python-plain": "Python", 
+        "java-plain": "Java", 
+        "javascript-plain": "JavaScript", 
+        "postgresql-plain": "PostgreSQL", 
+        "html5-plain": "HTML5", 
+        "css3-plain": "CSS3", 
+        "nodejs-plain": "Node.js", 
+        "express-original": "Express", 
+        "react-original": "React", 
+        "flask-original": "Flask", 
+        "firebase-plain": "Firebase", 
+        "bootstrap-plain": "Bootstrap", 
+        "pandas-plain": "pandas", 
+        "numpy-original": "NumPy", 
+        "git-plain": "Git", 
+        "docker-plain": "Docker",
+        "amazonwebservices-original": "AWS", 
+        "azure-plain": "Azure", 
+        "googlecloud-plain": "Google Cloud"
+    }
 
     return (
         <>
@@ -26,7 +39,7 @@ export default function App() {
 
             <div className="background">
                 <h1>BACKGROUND</h1>
-                <p>I recently graduated from the University of Guelph with a Bachelors of Computing in Computer Science
+                <p>I recently graduated from the University of Guelph with an honors degree in Computer Science
                     <br/><br/>As a software developer, I enjoy bridging the gap between developing and design -- combining my technical knowledge with my keen eye for design to create a beautiful product. My goal is to always build applications that are scalable and efficient under the hood while providing engaging, pixel-perfect user experiences.
                     <br/><br/>When I'm not in front of a computer screen. I'm probably reading books, travelling or playing video games.
                 </p>
@@ -35,29 +48,9 @@ export default function App() {
             <div className="skills">
                 <h1>SKILLS</h1>
                 <div className="icons">
-                    <i className={`devicon-c-plain ${isHovered ? 'colored' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></i>
-                    <i className={`devicon-python-plain ${isHovered ? 'colored' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></i>
-                    <i className={`devicon-java-plain ${isHovered ? 'colored' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></i>
-                    <i className={`devicon-javascript-plain ${isHovered ? 'colored' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></i>
-                    <i className={`devicon-postgresql-plain ${isHovered ? 'colored' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></i>
-                    <i className={`devicon-html5-plain ${isHovered ? 'colored' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></i>
-                    <i className={`devicon-css3-plain ${isHovered ? 'colored' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></i>
-                    <i className={`devicon-nodejs-plain ${isHovered ? 'colored' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></i>
-                    <i className={`devicon-express-original ${isHovered ? 'colored' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></i>
-                    <i className={`devicon-react-original ${isHovered ? 'colored' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></i>
-                    <i className={`devicon-flask-original ${isHovered ? 'colored' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></i>
-                    <i className={`devicon-mongodb-plain ${isHovered ? 'colored' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></i>
-                    <i className={`devicon-firebase-plain ${isHovered ? 'colored' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></i>
-                    <i className={`devicon-bootstrap-plain ${isHovered ? 'colored' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></i>
-                    <i className={`devicon-pandas-plain ${isHovered ? 'colored' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></i>
-                    <i className={`devicon-numpy-original ${isHovered ? 'colored' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></i>
-                    <i className={`devicon-git-plain ${isHovered ? 'colored' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></i>
-                    <i className={`devicon-docker-plain ${isHovered ? 'colored' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></i>
-                    <i className={`devicon-kubernetes-plain ${isHovered ? 'colored' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></i>
-                    <i className={`devicon-amazonwebservices-original ${isHovered ? 'colored' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></i>
-                    <i className={`devicon-azure-plain ${isHovered ? 'colored' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></i>
-                    <i className={`devicon-googlecloud-plain ${isHovered ? 'colored' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></i>
-          
+                    {Object.entries(iconsList).map(([iconKey, iconValue]) => (
+                        <Icon key={iconKey} iconKey={iconKey} iconValue={iconValue} />
+                    ))}
                 </div>
             </div>
 
---
+++ src/Icon.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export default function Icon({ iconKey, iconValue }) {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
+    return (
+        <i key={iconKey} className={`devicon-${iconKey} ${isHovered ? "colored" : ""}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <p>{iconValue}</p>
+        </i>
+    );
+}
