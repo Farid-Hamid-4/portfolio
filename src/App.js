59a15b3a46fb2681f90518cc8a609c65bd7fc27a
Farid-Hamid-4 <faridhamid00@gmail.com>
2023-08-21 20:02:47 -0400

    Low highlight under name

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import "bootstrap/dist/css/bootstrap.css";
-import { Container, Image } from "react-bootstrap";
-import me from "./me_cropped.png";
+import { Container} from "react-bootstrap";
+import "./App.css"
 
 export default function App() {
-  return (
-    <Container fluid>
-      <div style={{ backgroundColor: "red"}}>
-        <Image src={me} style={{ width : "30em" }}/>
-        <h1>Farid Hamid</h1>
-      </div>
-    </Container>
-  );
+    return (
+        <Container fluid>
+            <div id="introduction">
+               <h1>Hi! My name is Farid Hamid | Software Developer</h1>
+            </div>
+
+            <div id="background">
+                <h1>Background</h1>
+            </div>
+            
+            <div id="skills">
+                <h1>Skills</h1>
+            </div>
+
+            <div id="projects">
+                <h1>Projects</h1>
+            </div>
+        </Container>
+    );
 }
