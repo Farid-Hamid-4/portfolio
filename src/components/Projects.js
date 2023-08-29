7a5332ae8bb10b87348f5ee7f802818f5c86e58d
Farid Hamid <farid@seijuro.home>
2023-08-29 14:25:22 -0400

    Work on pc now

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,14 +16,14 @@ export default function Projects() {
             <div id="projects-container" className="section-margins">
                 <h1 className="section-title">Personal Projects</h1>
                 {projectsList.map((project, index) => (
-                    <div className="single-project">
+                    <div id="single-project">
                         <h2>{project.type}</h2>
                         <h1>{project.name}</h1>
-                        <div className="skills-used">
-                            {Object.entries(project.technologies).map(([techKey, techValue]) => <p>{techValue}</p>
+                        <ul id="skills-used">
+                            {Object.entries(project.technologies).map(([techKey, techValue]) => <li>{techValue}</li>
                             )}
-                        </div>
-                        <div className="project-links">
+                        </ul>
+                        <div id="project-links">
                             <Button variant="outline-dark" href={project.github} target="_blank" rel="noopener noreferrer">
                                 <i className="devicon-github-plain"></i>
                                 View Repo
