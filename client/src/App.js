6102aa7c37223b73328ee2fb88535dc3e07aa91a
Farid-Hamid-4 <faridhamid00@gmail.com>
2023-08-30 15:27:30 -0400

    Deploy server

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "devicon/devicon.min.css";
 
+// Import from library
+import { useEffect } from "react";
+
 // Components
 import Header from "./components/Header.js";
 import About from "./components/About.js";
@@ -12,6 +15,15 @@ import Footer from "./components/Footer.js";
 import "./styles/Global.css";
 
 export default function App() {
+
+    useEffect(() => {
+        fetch('https://main.d3nrpo7t1d7wxl.amplifyapp.com/get-access-token')
+        .then(response => response.json())
+        .then(data => {
+            console.log('Client ID:', data)
+        })
+    }, [])
+
     return (
         <>
             <Header />
