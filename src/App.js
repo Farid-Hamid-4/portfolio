f2d5a02f5b184122781e657f8a9fdf9c2ba82b77
Farid-Hamid-4 <faridhamid00@gmail.com>
2023-08-30 15:50:49 -0400

    Back to normal once again, but forgot to clean out old code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "devicon/devicon.min.css";
 
-// Import from library
-import { useEffect, useState } from "react";
-
 // Components
 import Header from "./components/Header.js";
 import About from "./components/About.js";
@@ -15,17 +12,8 @@ import Footer from "./components/Footer.js";
 import "./styles/Global.css";
 
 export default function App() {
-    const [data, setData] = useState(null);
-
-    useEffect(() => {
-      fetch("/api")
-        .then((res) => res.json())
-        .then((data) => setData(data.message));
-    }, []);
-
     return (
         <>
-            <div><h1>{!data ? "Loading..." : data}</h1></div>
             <Header />
             <About />
             <Skills />
--- src/components/Playlists.js
+++
@@ -1,6 +0,0 @@
-export default function Playlists() {
-
-    const clientId = process.env.REACT_APP_CLIENT_ID;
-
-    return;
-}
