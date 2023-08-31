b45e646f0f037015eaa02d27351b87597bf4c043
Farid-Hamid-4 <faridhamid00@gmail.com>
2023-08-31 12:04:22 -0400

    Implement the send message button action

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,14 @@ export default function About() {
             <div id="about-container" className="section-margins">
                 <div id="about-me-bio">
                     <h1 className="section-title">About Me</h1>
-                    <p>Hi! &#128075; My name is Farid Hamid and I'm a new grad &#127891; from the University of Guelph with an Honours degree in Computer Science.<br/><br/>I enjoy spending my free time listening to &#127911; music around the clock and playing &#127918; video games.<br/><br/>I believe one of the best ways to learn more about someone is to dive into their music catalog. Here are some of my playlists and the type of music I listen to:</p>
+                    <p>Hi! &#128075; My name is Farid Hamid and I'm a new grad &#127891; from the University of Guelph with an Honours degree in Computer Science.<br/><br/>I enjoy spending my free time listening to &#127911; music around the clock and playing &#127918; video games.<br/><br/>I believe one of the best ways to learn more about someone is to dive into their music catalog. Here are some of my playlists and the type of music I listen to</p>
+                    <div id="playlists-container">
+                        <iframe title="k-pop playlist by Farid Hamid" style={{borderRadius: "12px", width:"30%", height:"352px", frameBorder: "0" }} src="https://open.spotify.com/embed/playlist/2dCHx8JMQVnWJhRGu8idi9?utm_source=generator" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                        
+                        <iframe title="classics playlist by Farid Hamid" style={{ borderRadius: "12px", width:"30%", height:"352px", frameBorder: "0" }} src="https://open.spotify.com/embed/playlist/52VvgAKPbKpUogNeEriqTV?utm_source=generator" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+
+                        <iframe title="chill playlist by Farid Hamid" style={{borderRadius: "12px", width:"30%", height:"352px", frameBorder: "0" }} src="https://open.spotify.com/embed/playlist/3SOk5NdKaAYsPHcoUIZ9eQ?utm_source=generator" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                    </div>
                 </div>
                 <div id="about-me-image"><Image src={Farid}/></div>
             </div>
